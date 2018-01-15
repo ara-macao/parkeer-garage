b99d14908c94fe1d62a805eaead3dd5c378d7cbf
Thom van Dijk <thom.a.vandijk@gmail.com>
2018-01-15 17:47:03 +0100

    Setup for NetBeans.

---
+++ src/parksim/Main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parksim;
+
+/**
+ *
+ * @author Thom
+ */
+public class Main {
+    
+    private Simulator sim;
+    
+    
+    public static void main(String[] args) {
+        Simulator sim = new Simulator();
+        sim.run();
+    }
+}
