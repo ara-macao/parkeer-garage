63b147ef5f469d43e12f61aaf8142e1930864741
ara-macao <129383@gmail.com>
2018-01-25 02:15:14 +0100

    Implemented basic gui thread. ( cross thread access/tests still in local branch)

--- src/nl/parkingsimulator/main/MVCScreen.java
+++ src/nl/parkingsimulator/main/MVCScreen.java
@@ -1,7 +1,31 @@
 package nl.parkingsimulator.main;
 
+import javax.swing.*;
+
+
 public class MVCScreen {
+   public Thread guiThread;
+   
     public MVCScreen(){
         System.out.println("Emiel zijn screen zooi");
+        guiThread = new Thread() {
+            public void run() {
+                JFrame window = new JFrame();
+                JPanel content = new JPanel();
+                // implement interface view
+                window.add(content);
+                window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                window.pack();
+                
+                window.setVisible(true);
+            }
+        };
+        guiThread.start();
+        try {
+            guiThread.join();
+        } catch (InterruptedException e) {
+
+        }
     }
 }
+
